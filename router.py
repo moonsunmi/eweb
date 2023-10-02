72302d847ed9c27ba27ce49c0314ebf1127581ac
moonsunmi <celebmoon@naver.com>
2023-10-02 11:40:21 +0900

    Add Path class for path matching

--- router.py
+++ router.py
@@ -1,3 +1,14 @@
+class Path:
+    def __init__(self, path, func):
+        self.path = path
+        self.func = func
+
+    def isMatch(self, path):
+        if self.path == path:
+            return True
+        else:
+            return False
+
 
 class Router:
     def __init__(self, routes=None):
