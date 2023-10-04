87cf7ecfd3d4303ada33db5f61c6bd40abda10d4
moonsunmi <celebmoon@naver.com>
2023-10-04 17:25:21 +0900

    add App class to handle incoming request

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+from request import Request
+from router import Router
+from response import Response
+
 
 class App:
     def __init__(self):
-        pass
+        self.router = Router()
 
     def __call__(self, environ, start_response):
-        pass
+        try:
+            request = Request(environ, start_response)
+            func = self.router.get_route(request.path)
+            if func is not None:
+                response = func(request)
+
+        except:
+            pass
